aa47349dbd6b8070e61bcba83e9b92d1212c5fb6
Norman Walsh <ndw@nwalsh.com>
2016-01-27 17:58:28 -0600

    Suppress showing of trailing whitespace in dictionary buffer

--- helm-wordnet.el
+++ helm-wordnet.el
@@ -100,6 +100,7 @@
   "Display meaning of WORD."
   (let ((buf (get-buffer-create helm-wordnet-buffer)))
     (with-current-buffer buf
+      (setq show-trailing-whitespace nil)
       (read-only-mode -1)
       (erase-buffer)
       (setq cursor-type nil)
